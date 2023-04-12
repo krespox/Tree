4382b6c793c3f4da934b154364604dec8befcea7
Krystian <krystiankaaluzny@gmail.com>
2023-04-12 15:33:04 +0200

    This project consists of three classes: TreeNode, Tree, and Main. The TreeNode class represents a node in a binary tree and contains a value, a reference to the left child, and a reference to the right child. The Tree class represents a binary tree and provides methods to get the leaf nodes, the length of the longest path from the root to a leaf node, and to check if two trees are equivalent. The Main class creates two instances of Tree and performs some operations such as getting leaf nodes, finding the longest path, and checking if the two trees are equivalent.

---
+++ src/Main.java
@@ -0,0 +1,56 @@
+
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(5);
+        TreeNode node1 = new TreeNode(7);
+        TreeNode node2 = new TreeNode(3);
+        TreeNode node3 = new TreeNode(0);
+        TreeNode node4 = new TreeNode(8);
+        TreeNode node5 = new TreeNode(5);
+        TreeNode node6 = new TreeNode(2);
+        TreeNode node7 = new TreeNode(4);
+        TreeNode node8 = new TreeNode(6);
+
+        root.left = node1;
+        root.right = node2;
+        node1.left = node3;
+        node1.right = node4;
+        node4.left = node5;
+        node2.left = node6;
+        node2.right = node7;
+        node7.left = node8;
+
+        Tree tree = new Tree(root);
+        List<Integer> leafNodes = tree.getLeafNodes();
+        System.out.println("Leaf nodes: " + leafNodes);
+
+        int longestPath = tree.getLongestPath();
+        System.out.println("Longest path: " + longestPath);
+
+        TreeNode root2 = new TreeNode(5);
+        TreeNode node21 = new TreeNode(7);
+        TreeNode node22 = new TreeNode(3);
+        TreeNode node23 = new TreeNode(0);
+        TreeNode node24 = new TreeNode(8);
+        TreeNode node25 = new TreeNode(5);
+        TreeNode node26 = new TreeNode(2);
+        TreeNode node27 = new TreeNode(4);
+        TreeNode node28 = new TreeNode(6);
+
+        root2.left = node21;
+        root2.right = node22;
+        node21.left = node23;
+        node21.right = node24;
+        node24.left = node25;
+        node22.left = node26;
+        node22.right = node27;
+        node27.left = node28;
+
+        Tree tree2 = new Tree(root2);
+
+        boolean isEquivalent = tree.isEquivalent(tree2);
+        System.out.println("Equivalent trees: " + isEquivalent);
+    }
+}
---
+++ src/Tree.java
@@ -0,0 +1,56 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Tree {
+    private final TreeNode root;
+
+    public Tree(TreeNode root) {
+        this.root = root;
+    }
+
+    public List<Integer> getLeafNodes() {
+        List<Integer> leafNodes = new ArrayList<>();
+        getLeafNodes(root, leafNodes);
+        return leafNodes;
+    }
+
+    private void getLeafNodes(TreeNode node, List<Integer> leafNodes) {
+        if (node == null) {
+            return;
+        }
+        if (node.left == null && node.right == null) {
+            leafNodes.add(node.val);
+        }
+        getLeafNodes(node.left, leafNodes);
+        getLeafNodes(node.right, leafNodes);
+    }
+
+    public int getLongestPath() {
+        return getLongestPath(root);
+    }
+
+    private int getLongestPath(TreeNode node) {
+        if (node == null) {
+            return 0;
+        }
+        int leftHeight = getLongestPath(node.left);
+        int rightHeight = getLongestPath(node.right);
+        return Math.max(leftHeight, rightHeight) + 1;
+    }
+
+    public boolean isEquivalent(Tree other) {
+        return isEquivalent(this.root, other.root);
+    }
+
+    private boolean isEquivalent(TreeNode node1, TreeNode node2) {
+        if (node1 == null && node2 == null) {
+            return true;
+        } else if (node1 == null || node2 == null) {
+            return false;
+        }
+        if (node1.val != node2.val) {
+            return false;
+        }
+        return isEquivalent(node1.left, node2.left) && isEquivalent(node1.right, node2.right);
+    }
+}
